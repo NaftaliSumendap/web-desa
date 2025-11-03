d302838e40d4cb0dbce6200479ebb5416428eff0
mogus <sumendapn7@gmail.com>
2025-11-03 21:29:55 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,11 +23,30 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        if ($this->app->environment('production')) {
-            \URL::forceScheme('https');
+        // 3. Tambahkan kode ini
+        try {
+            // Ambil data desa (ID=1)
+            $desa = VillageProfile::firstOrCreate(['id' => 1]);
+            
+            // Bagikan variabel $desa ke SEMUA view
+            View::share('desa', $desa);
+
+        } catch (\Exception $e) {
+            // Tangani error jika database belum siap
+            View::share('desa', (object)[
+                'name' => 'Nama Desa',
+                'kabupaten' => 'Nama Kabupaten',
+                'logo' => null,
+                'address' => 'Alamat Kantor',
+                'kecamatan' => 'Nama Kecamatan',
+                'provinsi' => 'Nama Provinsi',
+                'telepon' => '08xxxxxxxx',
+                'email' => 'email@desa.id',
+            ]);
         }
     }
-   
+
+    
 }
